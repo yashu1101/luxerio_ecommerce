1a3a19cd8a61cf8b1d0addb34a4db74fc8187fbe
Yash Choudhary <yashchoudhary1101@gmail.com>
2026-06-30 16:03:36 +0530

    fixed nodemailer configuration

--- Backend/src/services/mailer.service.js
+++ Backend/src/services/mailer.service.js
@@ -4,11 +4,17 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
 
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.APP_USER,
         pass: process.env.APP_PASS,
     },
+    tls: {
+       
+        rejectUnauthorized: false 
+    }
 
 
 });
